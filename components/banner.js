44c4d5e34e23c76ec0aced74dc3128c53b50c7da
Peter Ihimire <peterihimire@gmail.com>
2020-12-12 17:29:27 +0100

    Still working on the typewriter effect, but I am having issues with the cursor beign on the next line, when using headings, but with only strings, there seems to be no issue

--- components/banner.js
+++ components/banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Typewriter from "typewriter-effect";
 
 const Banner = () => {
   return (
@@ -7,10 +8,19 @@ const Banner = () => {
       <div className="row full-height">
         <div className="container full-height">
           <div className="col s12 pad0">
-            <div className="col s12 m7">
+            <div className="col s12 m5">
               <div className="main-text-container">
-                <h1>Hi, I'm Peter Ihimire</h1>
-                <p>I'm, a front-end web developer</p>
+                <Typewriter
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString(
+                        '<h1>Peter <span style="color:  #008ed6">Ihimire</span></h1>'
+                      )
+                      .pauseFor(1500)
+                      .typeString("<h4>Front-End Web Developer</h4>")
+                      .start();
+                  }}
+                />
                 <div className="btn">
                   <Link href="/portfolio" className="">
                     <a>projects</a>
@@ -18,7 +28,7 @@ const Banner = () => {
                 </div>
               </div>
             </div>
-            <div className="col s12 m5 offset-m0">
+            <div className="col s12 m5 offset-m2">
               <div className="rel righ-align">
                 <img
                   src="/undraw-code-3.svg"
