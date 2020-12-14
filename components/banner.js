cf3951fb70170dae91207506c51edef1acf16e18
Peter Ihimire <peterihimire@gmail.com>
2020-12-14 22:08:08 +0100

    Fixed the btn colors, arranged the headings and the image

--- components/banner.js
+++ components/banner.js
@@ -8,28 +8,28 @@ const Banner = () => {
       <div className="row full-height">
         <div className="container full-height">
           <div className=" col s12 pad0">
-            <div className="col s12 m5">
+            <div className="col s12 m7">
               <div className="main-text-container">
                 <Typewriter
                   onInit={(typewriter) => {
                     typewriter
                       .typeString(
-                        '<h1>Peter <span style="color:  #3187e9">Ihimire</span></h1>'
+                        '<h1>Peter <span style="color: #3187e9">Ihimire</span></h1>'
                       )
                       .pauseFor(1500)
                       .typeString("<h4>Front-End Web Developer</h4>")
                       .start();
                   }}
                 />
-                <div className="btn">
-                  <Link href="/portfolio" className="btn">
+                <button className="btn btn-bigger">
+                  <Link href="/portfolio" className=" btn-link-color">
                     <a>projects</a>
                   </Link>
-                </div>
+                </button>
               </div>
             </div>
-            <div className="col s12 m5 offset-m2">
-              <div className="rel righ-align">
+            <div className="col s12 m5 offset-m0">
+              <div className="rel righ-align main-img-container">
                 <img
                   src="/undraw-code-3.svg"
                   alt="programmer"
