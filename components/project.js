9d491ebe5a2c5acf6fbbf1843ac29a160c93eb6d
Peter Ihimire <peterihimire@gmail.com>
2020-12-17 17:31:50 +0100

    Working on the project and projectlist components to use arrays b4 I start using get initial props

--- components/project.js
+++ components/project.js
@@ -2,70 +2,23 @@ import React, { Component } from "react";
 import Link from "next/link";
 
 class Project extends Component {
-  state = {
-    projects: [
-      {
-        title: "benkih ecommerce",
-        detail:
-          "Our delivery process can take place from minutes to few hours depending on your locationlorem20 Our delivery process can take place from minutes to few hours depending on your location ",
-        link: "https://benkih-ecommerce-react.netlify.app",
-        image: "https://cloudinary.com/benkih-ecommerce",
-      },
-      {
-        title: "benkih blog",
-        detail:
-          "Our delivery process can take place from minutes to few hours depending on your locationlorem20 Our delivery process can take place from minutes to few hours depending on your location ",
-        link: "https://benkih-ecommerce-react.netlify.app",
-        image: "https://cloudinary.com/benkih-ecommerce",
-      },
-      {
-        title: "portfolio website",
-        detail:
-          "Our delivery process can take place from minutes to few hours depending on your locationlorem20 Our delivery process can take place from minutes to few hours depending on your location ",
-        link: "https://benkih-ecommerce-react.netlify.app",
-        image: "https://cloudinary.com/benkih-ecommerce",
-      },
-      {
-        title: "benkih quiz",
-        detail:
-          "Our delivery process can take place from minutes to few hours depending on your locationlorem20 Our delivery process can take place from minutes to few hours depending on your location ",
-        link: "https://benkih-ecommerce-react.netlify.app",
-        image: "https://cloudinary.com/benkih-ecommerce",
-      },
-      {
-        title: "benkih todo",
-        detail:
-          "Our delivery process can take place from minutes to few hours depending on your locationlorem20 Our delivery process can take place from minutes to few hours depending on your location ",
-        link: "https://benkih-ecommerce-react.netlify.app",
-        image: "https://cloudinary.com/benkih-ecommerce",
-      },
-    ],
-  };
 
   render() {
-    console.log(this.state.projects);
+    console.log(this.props);
     return (
-      // <article className="project">
-      //   <div className="img-container">
-      //     <img src="/product8.png" alt="project" />
-      //   </div>
-      //   <div className="project-footer">
-      //     <p>benkih-ecommerce</p>
-      //     <h5>&copy;</h5>
-      //   </div>
-      // </article>
+   
       <>
-        {/* <div className="project-hover"> <p>{this.state.projects.detail}</p> </div> */}
+        {/* <div className="project-hover">{this.props.projectAll.detail}</div> */}
         <article className="project">
           <div className="img-container">
-            <img src={this.state.projects.image} alt="project" />
+            <img src={this.props.projectAll.image} alt="project" />
           </div>
           <div className="project-footer">
-            <p>{this.state.projects.title}</p>
+            <p>{this.props.projectAll.title}</p>
             <div>
-              {/* <Link href={this.state.projects.link}>
+              <Link href={this.props.projectAll.link}>
                 <a>&copy;</a>
-              </Link> */}
+              </Link>
             </div>
           </div>
         </article>
--- components/projectList.js
+++ components/projectList.js
@@ -1,20 +1,63 @@
-import React from "react";
+import React, { Component } from "react";
+import Link from "next/link";
 import Project from "../components/project";
 
-const projectList = () => {
-  return (
-    <div className="project-list">
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-    </div>
-  );
-};
+class ProjectList extends Component {
+  state = {
+    projects: [
+      {
+        id: 1,
+        title: "benkih ecommerce",
+        detail:
+          "Our delivery process can take place from minutes to few hours depending on your locationlorem20 Our delivery process can take place from minutes to few hours depending on your location ",
+        link: "https://benkih-ecommerce-react.netlify.app",
+        image: "https://cloudinary.com/benkih-ecommerce",
+      },
+      {
+        id: 2,
+        title: "benkih blog",
+        detail:
+          "Our delivery process can take place from minutes to few hours depending on your locationlorem20 Our delivery process can take place from minutes to few hours depending on your location ",
+        link: "https://benkih-ecommerce-react.netlify.app",
+        image: "https://cloudinary.com/benkih-ecommerce",
+      },
+      {
+        id: 3,
+        title: "portfolio website",
+        detail:
+          "Our delivery process can take place from minutes to few hours depending on your locationlorem20 Our delivery process can take place from minutes to few hours depending on your location ",
+        link: "https://benkih-ecommerce-react.netlify.app",
+        image: "https://cloudinary.com/benkih-ecommerce",
+      },
+      {
+        id: 4,
+        title: "benkih quiz",
+        detail:
+          "Our delivery process can take place from minutes to few hours depending on your locationlorem20 Our delivery process can take place from minutes to few hours depending on your location ",
+        link: "https://benkih-ecommerce-react.netlify.app",
+        image: "https://cloudinary.com/benkih-ecommerce",
+      },
+      {
+        id: 5,
+        title: "benkih todo",
+        detail:
+          "Our delivery process can take place from minutes to few hours depending on your locationlorem20 Our delivery process can take place from minutes to few hours depending on your location ",
+        link: "https://benkih-ecommerce-react.netlify.app",
+        image: "https://cloudinary.com/benkih-ecommerce",
+      },
+    ],
+  };
 
-export default projectList;
+  render() {
+    console.log(this.state.projects);
+    return (
+      <div className="project-list">
+        {this.state.projects.map((project) => {
+          return <Project key={project.id} projectAll={project} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default ProjectList;
