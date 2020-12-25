b9ce27d08b7b0b42067caca9dcdec97c2b8ca7e3
Peter Ihimire <peterihimire@gmail.com>
2020-12-25 21:50:02 +0100

    Worked on the project-hover state , so that when a project is hovered it will show the project details

--- components/project.js
+++ components/project.js
@@ -1,55 +1,20 @@
-// import React, { Component } from "react";
-// import Link from "next/link";
-
-// class Project extends Component {
-
-//   render() {
-//     console.log(this.props.bookAll);
-//     return (
-   
-//       <>
-//         {/* <div className="project-hover">{this.props.projectAll.detail}</div> */}
-//         <article className="project">
-//           <div className="img-container">
-//             <img src={this.props.projectAll.image} alt="project" />
-//           </div>
-//           <div className="project-footer">
-//             <p>{this.props.projectAll.title}</p>
-//             <div>
-//               <Link href={this.props.projectAll.link}>
-//                 <a>&copy;</a>
-//               </Link>
-//             </div>
-//           </div>
-//         </article>
-//       </>
-//     );
-//   }
-// }
-
-// export default Project;
-
-
-
 import React, { Component } from "react";
 import Link from "next/link";
 
 class Project extends Component {
-
   render() {
     console.log(this.props.bookAll);
     return (
-   
       <>
-        {/* <div className="project-hover">{this.props.projectAll.detail}</div> */}
         <article className="project">
           <div className="img-container">
-            <img src='/product8.png' alt="project" />
+            <img src={this.props.projectAll.image} alt="project" />
+            <div className="project-hover">{this.props.projectAll.detail}</div>
           </div>
           <div className="project-footer">
-            <p>{this.props.bookAll.title}</p>
+            <p>{this.props.projectAll.title}</p>
             <div>
-              <Link href={this.props.bookAll.id}>
+              <Link href={this.props.projectAll.link}>
                 <a>&copy;</a>
               </Link>
             </div>
@@ -61,3 +26,34 @@ class Project extends Component {
 }
 
 export default Project;
+
+// import React, { Component } from "react";
+// import Link from "next/link";
+
+// class Project extends Component {
+
+//   render() {
+//     console.log(this.props.bookAll);
+//     return (
+
+//       <>
+//         {/* <div className="project-hover">{this.props.projectAll.detail}</div> */}
+//         <article className="project">
+//           <div className="img-container">
+//             <img src='/product8.png' alt="project" />
+//           </div>
+//           <div className="project-footer">
+//             <p>{this.props.bookAll.title}</p>
+//             <div>
+//               <Link href={this.props.bookAll.id}>
+//                 <a>&copy;</a>
+//               </Link>
+//             </div>
+//           </div>
+//         </article>
+//       </>
+//     );
+//   }
+// }
+
+// export default Project;
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,28 +2,28 @@ import React from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import "materialize-css/dist/css/materialize.min.css";
-// import ProjectList from "../components/projectList";
-import Project from "../components/project";
+import ProjectList from "../components/projectList";
+// import Project from "../components/project";
 
 const Portfolio = ({ results }) => {
-  console.log(results);
-  let bookList = results.map((item) => {
-    let id = item.id;
-    let title = item.volumeInfo.title;
-    // let authors = item.volumeInfo.authors;
-    // let description = item.volumeInfo.description;
+  // console.log(results);
+  // let bookList = results.map((item) => {
+  //   let id = item.id;
+  //   let title = item.volumeInfo.title;
+  //   // let authors = item.volumeInfo.authors;
+  //   // let description = item.volumeInfo.description;
 
-    let book = {
-      id,
-      title,
-      // authors,
-      // description,
-    };
-    console.log(book);
+  //   let book = {
+  //     id,
+  //     title,
+  //     // authors,
+  //     // description,
+  //   };
+  //   console.log(book);
 
-    return book;
-  });
-  console.log(bookList);
+  //   return book;
+  // });
+  // console.log(bookList);
   return (
     <div className="portfolio">
       <Head>
@@ -38,12 +38,12 @@ const Portfolio = ({ results }) => {
               my <span> portfolio</span>
             </h1>
             <div className="portfolio-content">
-              <div className="project-list">
-                {/* <ProjectList /> */}
-                {bookList.map((book) => {
+              {/* <div className="project-list"> */}
+                <ProjectList />
+                {/* {bookList.map((book) => {
                   return <Project key={book.id} bookAll={book} />;
-                })}
-              </div>
+                })} */}
+              {/* </div> */}
             </div>
           </div>
         </div>
@@ -52,41 +52,24 @@ const Portfolio = ({ results }) => {
   );
 };
 
-export async function getServerSideProps(context) {
-  const res = await fetch(
-    "https://www.googleapis.com/books/v1/volumes?q=isbn:9783161484100"
-  );
-  console.log(res);
-  const json = await res.json();
-  console.log(json);
-  const books = json;
+// export async function getServerSideProps(context) {
+//   const res = await fetch(
+//     "https://www.googleapis.com/books/v1/volumes?q=isbn:9783161484100"
+//   );
+//   console.log(res);
+//   const json = await res.json();
+//   console.log(json);
+//   const books = json;
 
-  // let bookList = await books.items.map((item) => {
-  //   let id = item.id;
-  //   let title = item.volumeInfo.title;
-  //   let authors = item.volumeInfo.authors;
-  //   let description = item.volumeInfo.description;
-
-  //   let book = {
-  //     id,
-  //     title,
-  //     authors,
-  //     description,
-  //   };
-  //   return book
-  // });
-
-  // console.log(bookList);
-  // const allBooks = await bookList;
-  if (!json) {
-    return {
-      notFound: true,
-    };
-  }
-  return {
-    props: {
-      results: books.items,
-    },
-  };
-}
+//   if (!json) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+//   return {
+//     props: {
+//       results: books.items,
+//     },
+//   };
+// }
 export default Portfolio;
