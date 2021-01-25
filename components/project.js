fcb60641a927cea1e9653cc8b79e3275900120da
Peter Ihimire <peterihimire@gmail.com>
2021-01-25 22:59:49 +0100

    Added react reveal package into my project, yet to add loading animations

--- components/project.js
+++ components/project.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import { FaLink } from "react-icons/fa";
+import Zoom from "react-reveal/Zoom";
 
 class Project extends Component {
   render() {
     console.log(this.props);
     return (
       <>
-        <article className="project">
-          <div className="img-container">
-            <img src={this.props.image} alt="project" />
-            <div className="project-hover">{this.props.detail}</div>
-          </div>
-          <div className="project-footer">
-            <p>{this.props.title}</p>
-            <div className="fa-link-div">
-              <a
-                href={this.props.online}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="project-link"
-              >
-                <FaLink className="fa-link" />
-              </a>
+        <Zoom>
+          <article className="project">
+            <div className="img-container">
+              <img src={this.props.image} alt="project" />
+              <div className="project-hover">{this.props.detail}</div>
             </div>
-          </div>
-        </article>
+            <div className="project-footer">
+              <p>{this.props.title}</p>
+              <div className="fa-link-div">
+                <a
+                  href={this.props.online}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="project-link"
+                >
+                  <FaLink className="fa-link" />
+                </a>
+              </div>
+            </div>
+          </article>
+        </Zoom>
       </>
     );
   }
